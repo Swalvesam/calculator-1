00acefd7340ffb518e06dbe85af34a03b9d2f41f
Samantha Swalve <samantha.swalve@gmail.com>
2021-04-28 19:02:28 +0000

    Adding calculator to repo

--- calculator.py
+++ calculator.py
@@ -35,6 +35,7 @@ while True:
         print("Those aren't numbers!")
         continue
 
+
     # We have to cast each value we pass to an arithmetic function from a
     # a string into a numeric type. If we use float across the board, all
     # results will have decimal points, so let's do that for consistency.
